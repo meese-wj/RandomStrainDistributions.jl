5d99dcb0ce807d80d7e9386c43a64915d7d73228
W. Joe Meese <meese022@umn.edu>
2022-04-04 21:25:21 -0500

    Add `jldoctest`s for the shear functions

--- src/RandomStrainDistributions.jl
+++ src/RandomStrainDistributions.jl
@@ -19,6 +19,12 @@ Calculate the ``B_{1g}`` from an edge dislocation for a given Burgers vector `bo
 # Additional Information
 * The edge dislocation is aligned along the ``z`` axis at the origin.
 * This is in units of ``1/(1-σ)`` for a Poisson ratio ``σ``.
+
+# Examples
+```jldocstest
+julia> b1g_shear( Vector2D(1.,1.), Vector2D(1.,0.) )
+-0.07957747154594767
+```
 """
 function b1g_shear( eval_r::Vector2D, bob::Vector2D )
     return -1/π * ( eval_r.vec[1] * eval_r.vec[2] / magnitude2(eval_r) ) * (bob ⋅ eval_r) / magnitude2(eval_r)
@@ -32,6 +38,12 @@ Calculate the ``B_{2g}`` from an edge dislocation for a given Burgers vector `bo
 # Additional Information
 * The edge dislocation is aligned along the ``z`` axis at the origin.
 * This is in units of ``1/(1-σ)`` for a Poisson ratio ``σ``.
+
+# Examples
+```jldocstest
+julia> b2g_shear( Vector2D(1.,1.), Vector2D(1.,0.) )
+0.15915494309189535
+```
 """
 function b2g_shear( bob::Vector2D, eval_r::Vector2D )
     return 1/(2*π) * ( ( eval_r.vec[1]^2 - eval_r.vec[2]^2 ) / magnitude2(eval_r) ) * (bob ⋅ eval_r) / magnitude2(eval_r)
@@ -45,6 +57,23 @@ location and aligned along the ``z`` axis with a Burger's vector `bob`.
 
 # Additional Information
 * This function mutates both `shears` and `eval_r`. It should be used in cases where `eval_r` is a temporary vector.
+
+# Examples
+```jldocstest
+julia> using StaticArrays
+
+julia> A = MVector{2}(0., 0.)
+2-element MVector{2, Float64} with indices SOneTo(2):
+ 0.0
+ 0.0
+
+julia> bxg_shears!( A, Vector2D(1.,1.), Vector2D(1.,0.) )
+
+julia> A
+2-element MVector{2, Float64} with indices SOneTo(2):
+ -0.07957747154594767
+  0.15915494309189535
+```
 """
 function bxg_shears!( shears::MVector{2}, eval_r::Vector2D, bob::Vector2D; diff::Function = subtract!, source_r::Vector2D = Vector2D(0.,0.) )
     vector_diff!(eval_r, source_r; diff = diff)

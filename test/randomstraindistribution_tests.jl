244285044916cc6a9a66be0ea63ef1dc59c82f9c
W. Joe Meese <meese022@umn.edu>
2022-11-08 12:21:01 -0600

    Add more distribution tests

--- test/randomstraindistribution_tests.jl
+++ test/randomstraindistribution_tests.jl
@@ -18,10 +18,10 @@ end
 
     let 
         @testset "Testing Random Dislocations" begin
-            burgers_vectors = tetragonal_burgers_vectors
             ςon = 1/50
             Lx = 100
-            rsd = RandomDislocationDistribution(; expected_num_dislocations = round(Int, ςon * Lx^2), Lx = Lx, burgers_vectors = burgers_vectors )
+            Ly = Lx
+            rsd = RandomDislocationDistribution(; expected_num_dislocations = round(Int, ςon * Lx^2), Lx = Lx, Ly = Ly )
             all_dis = collect_dislocations(rsd)
             
             @testset "Topological charge sums to zero" begin
@@ -29,10 +29,46 @@ end
                 @test isequal( sum( all_dis[Int(BurgersVector), :] ), Vector2D(0., 0.) )
             end
 
+            @testset "Burgers Vector selection" begin
+                # this tests that the only Burgers vectors selected are those provided
+                # in the tetragonal_burgers_vectors
+                those_provided = true
+                for rand_bv ∈ all_dis[Int(BurgersVector), :]
+                    count = zero(Int)
+                    for provided_bv ∈ tetragonal_burgers_vectors
+                        count += isequal(rand_bv, provided_bv) ? one(count) : zero(count)
+                    end
+                    
+                    if count != one(count)
+                        those_provided = false
+                    end
+                end
+
+                @test those_provided
+            end
+
             @testset "All origins are unique" begin
                 # this makes sure no two dislocations sit on top of one another
                 @test length( unique_origins( all_dis[Int(DislocationOrigin), :] ) ) == size(all_dis)[2]
             end
+
+            @testset "All origins are within the grid" begin
+                # this makes sure no dislocations were generated outside the system
+                # and that no dislocations are centered on the grid itself.
+                all_inside = true
+                no_grid_touching = true
+                for origin ∈ all_dis[Int(DislocationOrigin), :]
+                    if !( 0 < origin.vec[1] < Lx + one(Lx) && 0 < origin.vec[2] < Ly + one(Ly) )
+                        all_inside = false
+                    end
+                    if ( origin.vec[1] - floor(origin.vec[1]) ≈ zero(origin.vec[1]) ) && ( origin.vec[2] - floor(origin.vec[2]) ≈ zero(origin.vec[2]) )
+                        no_grid_touching == false
+                    end
+                end
+                @test all_inside
+                @test no_grid_touching
+            end
+
 
         end
     end

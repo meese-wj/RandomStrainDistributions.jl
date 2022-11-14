37a9e50859361bb5d4233047b04d64520053a27a
W. Joe Meese <meese022@umn.edu>
2022-11-14 16:02:07 -0600

    Converges when the total Burgers vector vanishes

--- src/PBCImageFields.jl
+++ src/PBCImageFields.jl
@@ -8,10 +8,19 @@ module PBCImageFields
 
 using ..PhysicalVectors
 
-export PBCField
+export PBCField, image_origin
 
 """
-    PBCField(ϕfunc::Function, position, Lx, Ly, [tolerance])
+    image_origin(origin, nx, ny, Lx, Ly)
+
+Compute the origin for the image dislocation in the (nx, ny) image cell.
+"""
+image_origin( origin, nx, ny, Lx, Ly, xhat = Vector2D(1.0, 0.0), yhat = Vector2D(0.0, 1.0) ) = origin + nx * Lx * xhat + ny * Ly * yhat
+
+image_contribution( ϕfunc::Function, position::Vector2D{T}, _image_origin ) where T = ( ϕfunc( position, _image_origin ) )::T
+
+"""
+    PBCField(ϕfunc::Function, position, origins, Lx, Ly, [tolerance])
 
 Calculate the `ϕfunc`tion on a square periodic lattice of
 size Lx, Ly. 
@@ -26,9 +35,9 @@ size Lx, Ly.
     square trajectory should converge. If one supplies a divergent
     field, however, then this sum will always clearly diverge.
 """
-function PBCField(ϕfunc::Function, position, origin::Vector2D, Lx, Ly, tolerance = sqrt(eps()) )
+function PBCField(ϕfunc::Function, position::Vector2D{T}, origins, Lx, Ly, tolerance = sqrt(eps()) ) where T
     # First evaluate the field within its own grid
-    output = ϕfunc(position, origin)
+    output::T = ϕfunc(position, origins)
     
     # Now start computing along square trajectories
     old_output = output
@@ -36,58 +45,36 @@ function PBCField(ϕfunc::Function, position, origin::Vector2D, Lx, Ly, toleranc
     not_complete = true
     while ( not_complete || square_index == zero(Int) )
         square_index += one(square_index)
-        
-        # max_abs_out = zero(output)
         square_total = zero(output)
 
-        @show square_index, output
         # Start in top left and move down at constant x 
         left_xdx = -square_index
         for ydx ∈ range(-square_index, square_index, step = 1)
-            image_origin = origin + left_xdx * Lx * Vector2D( 1.0, 0.0 ) + ydx * Ly * Vector2D( 0.0, 1.0 )
-            new_out = ϕfunc(position, image_origin)
-            square_total += new_out
-            # max_abs_out = abs(new_out) > max_abs_out ? abs(new_out) : max_abs_out
+            square_total += image_contribution( ϕfunc, position, image_origin.(origins, left_xdx, ydx, Lx, Ly) )
         end
 
         # Now move along the bottom edge at constant y
         bottom_ydx = -square_index
         for xdx ∈ range(-square_index, square_index, step = 1)
-            image_origin = origin + xdx * Lx * Vector2D( 1.0, 0.0 ) + bottom_ydx * Ly * Vector2D( 0.0, 1.0 )
-            new_out = ϕfunc(position, image_origin)
-            square_total += new_out
-            # max_abs_out = abs(new_out) > max_abs_out ? abs(new_out) : max_abs_out
+            square_total += image_contribution( ϕfunc, position, image_origin.(origins, xdx, bottom_ydx, Lx, Ly) )
         end
         
         # Next move along the right edge at constant x
         right_xdx = square_index
         for ydx ∈ range(square_index, -square_index, step = 1)
-            image_origin = origin + right_xdx * Lx * Vector2D( 1.0, 0.0 ) + ydx * Ly * Vector2D( 0.0, 1.0 )
-            new_out = ϕfunc(position, image_origin)
-            square_total += new_out
-            # max_abs_out = abs(new_out) > max_abs_out ? abs(new_out) : max_abs_out
+            square_total += image_contribution(ϕfunc, position, image_origin.(origins, right_xdx, ydx, Lx, Ly))
         end
         
         # Finally move along the top edge at constant y
         top_ydx = square_index
         for xdx ∈ range(square_index, -square_index, step = 1)
-            image_origin = origin + xdx * Lx * Vector2D( 1.0, 0.0 ) + top_ydx * Ly * Vector2D( 0.0, 1.0 )
-            new_out = ϕfunc(position, image_origin)
-            square_total += new_out
-            # max_abs_out = abs(new_out) > max_abs_out ? abs(new_out) : max_abs_out
+            square_total += image_contribution(ϕfunc, position, image_origin.(origins, xdx, top_ydx, Lx, Ly))
         end
 
-        # Now check to see if the cutoff (to within a tolerance)
-        # was reached. This is done by multiplying the perimeter by
-        # the maximum bound. If this is smaller than the tolerance * output
-        # then the next iteration will definitely be convergent and we end
-        # the summation.
-        # @show perimeter = 4 * ( 2 * square_index + 1 )
-        # @show max_bound = perimeter * max_abs_out
-        # @show complete = max_bound / abs(output) < tolerance
-        @show square_total
-        @show output += square_total
-        @show complete = abs(square_total) / abs(output) < tolerance
+        # Now check that the contribution along the square is smaller
+        # than the output * tolerance (in absolute scale)
+        output += square_total
+        complete = abs(square_total) / abs(output) < tolerance
 
         not_complete = !complete
     end

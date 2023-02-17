91f05560dbc2b23e4c3782a20d4cc9746ed170f1
W. Joe Meese <meese022@umn.edu>
2023-02-17 11:11:50 -0600

    Write a script to collect and write out the DataFrame

---
+++ scripts/converttoDataFrames.jl
@@ -0,0 +1,16 @@
+
+using DrWatson
+using CSV
+include("src/datatoDataFrames.jl")
+
+function convertToDataFrames(dir, filesavename)
+    filenames = load_dataset_names(dir)
+    @info "Loading data as a single DataFrame"
+    df = find_DataFrames(filenames)
+    write_file = joinpath(dir, "DataFrames", filesavename)
+    @info "Writing single DataFrame as $(write_file)"
+    CSV.write(df, write_file)
+    return df
+end
+
+@time convertToDataFrames(datadir(), "unit_cratio_sweep.csv")
